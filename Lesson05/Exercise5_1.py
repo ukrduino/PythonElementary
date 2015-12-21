9a5fb26e21cdffe25db05f1c826671859965b5e0
Sergey Romanyuk <ukrduino@gmail.com>
2015-12-21 16:57:47 +0200

    5 урок

--- Lesson05/Exercise5_1.py
+++ Lesson05/Exercise5_1.py
@@ -14,61 +14,47 @@ import math
 
 class Vector(object):
     """
-    Vector class representing two-dimensional vector (with x and y coordinates)
-    which supports the following operations:
-     1. Addition of two vectors: v1 + v2
-     2. Subtraction of two vectors: v1 - v2
-     3. Dot product of two vectors:
-         a. Using operator v1 * v2
-         b. Using method v1.dot(v2)
-         c. Using class method Vector2d.dot(v1, v2)
-     4. Vector length property:
-         a. v.length
-     5. String representation: str(v)
      >>> vector2 = Vector(3, 4)
      >>> vector2.length
      5.0
      >>> vector1 = Vector(1, 3)
      >>> vector1
-     <Vector:1.0,3.0>
+     <Vector:1,3>
      >>> print str(vector1)
-     1.0,3.0
+     1,3
      >>> vector3 = vector1 + vector2
      >>> print vector3
-     4.0,7.0
+     4,7
      >>> vector4 = vector1 - vector2
      >>> print vector4
-     -2.0,-1.0
+     -2,-1
      >>> print vector1 * vector2
-     15.0
+     15
      >>> print Vector.dot(vector1,vector2)
-     15.0
+     15
      >>> print vector1.dot(vector2)
-     15.0
+     15
     """
 
     def __init__(self, x, y):
-        self.x = float(x)
-        self.y = float(y)
+        self.x = x
+        self.y = y
         self.length = math.sqrt(self.x**2 + self.y**2)
 
     def __add__(self, other):
-        if type(other) == type(self):
-            new_x = self.x + other.x
-            new_y = self.y + other.y
-            return Vector(new_x, new_y)
+        new_x = self.x + other.x
+        new_y = self.y + other.y
+        return Vector(new_x, new_y)
 
     def __sub__(self, other):
-        if type(other) == type(self):
-            new_x = self.x - other.x
-            new_y = self.y - other.y
-            return Vector(new_x, new_y)
+        new_x = self.x - other.x
+        new_y = self.y - other.y
+        return Vector(new_x, new_y)
 
     def __mul__(self, other):
-        if type(other) == type(self):
-            new_x = self.x * other.x
-            new_y = self.y * other.y
-            return new_x + new_y
+        new_x = self.x * other.x
+        new_y = self.y * other.y
+        return new_x + new_y
 
     @classmethod
     def dot(cls, v1, v2):
@@ -77,10 +63,9 @@ class Vector(object):
         return new_x + new_y
 
     def dot(self, other):
-        if type(other) == type(self):
-            new_x = self.x * other.x
-            new_y = self.y * other.y
-            return new_x + new_y
+        new_x = self.x * other.x
+        new_y = self.y * other.y
+        return new_x + new_y
 
     def __repr__(self):
         class_name = self.__class__.__name__
--- Lesson05/Exercise5_2.py
+++ Lesson05/Exercise5_2.py
@@ -10,3 +10,123 @@
 # cast only if there is enough mana. Use magic mixins to reduce repetitive code.
 
 
+class ArcaneMagicMixin(object):
+    arcane_magic_word = "black hole"
+    arcane_magic_cost = 5
+
+
+class FireMagicMixin(object):
+    fire_magic_word = "fireball"
+    fire_magic_cost = 10
+
+
+class IceMagicMixin(object):
+    ice_magic_word = "freeze"
+    ice_magic_cost = 15
+
+
+class Hero(object):
+    """
+    Base Hero class
+    """
+    def __init__(self, weapon=None):
+        self.mana = 100
+        self.health = 200
+        self.weapon = weapon
+
+    def use_magic(self, magic_word, magic_cost):
+        if self.mana > magic_cost:
+            print self.__class__.__name__ + " said '" + magic_word + "'"
+            self.mana -= magic_cost
+        else:
+            print "NOT ENOUGH MANA!!! YOU HAVE {} but required {}".format(self.mana, magic_cost)
+
+    def attack(self):
+        print self.__class__.__name__ + " used '" + self.weapon + "'"
+
+
+class Warrior(FireMagicMixin, Hero):
+    """
+    >>> w = Warrior();
+    >>> w.health
+    200
+    >>> w.mana
+    100
+    >>> w.fire_magic_word
+    'fireball'
+    >>> w.fire_magic_cost
+    10
+    >>> w.use_magic(w.fire_magic_word, w.fire_magic_cost)
+    Warrior said 'fireball'
+    >>> w.mana
+    90
+    >>> w.attack()
+    Warrior used 'hammer'
+    """
+    def __init__(self):
+        super(Warrior, self).__init__("hammer")
+
+
+class Mag(IceMagicMixin, ArcaneMagicMixin, FireMagicMixin, Hero):
+    """
+    >>> m = Mag();
+    >>> m.health
+    200
+    >>> m.mana
+    100
+    >>> m.fire_magic_word
+    'fireball'
+    >>> m.fire_magic_cost
+    10
+    >>> m.ice_magic_word
+    'freeze'
+    >>> m.ice_magic_cost
+    15
+    >>> m.arcane_magic_word
+    'black hole'
+    >>> m.arcane_magic_cost
+    5
+    >>> m.use_magic(m.arcane_magic_word, m.arcane_magic_cost)
+    Mag said 'black hole'
+    >>> m.mana
+    95
+    >>> m.use_magic(m.fire_magic_word, m.fire_magic_cost)
+    Mag said 'fireball'
+    >>> m.mana
+    85
+    >>> m.use_magic(m.ice_magic_word, m.ice_magic_cost)
+    Mag said 'freeze'
+    >>> m.mana
+    70
+    >>> m.use_magic(m.ice_magic_word, m.ice_magic_cost)
+    Mag said 'freeze'
+    >>> m.use_magic(m.ice_magic_word, m.ice_magic_cost)
+    Mag said 'freeze'
+    >>> m.use_magic(m.ice_magic_word, m.ice_magic_cost)
+    Mag said 'freeze'
+    >>> m.use_magic(m.ice_magic_word, m.ice_magic_cost)
+    Mag said 'freeze'
+    >>> m.use_magic(m.ice_magic_word, m.ice_magic_cost)
+    NOT ENOUGH MANA!!! YOU HAVE 10 but required 15
+    """
+    def __init__(self):
+        super(Mag, self).__init__()
+
+
+class Archer(IceMagicMixin, Hero):
+    """
+    >>> a = Archer()
+    >>> a.use_magic(a.ice_magic_word, a.ice_magic_cost)
+    Archer said 'freeze'
+    >>> a.mana
+    85
+    >>> a.attack()
+    Archer used 'arrows'
+    """
+    def __init__(self):
+        super(Archer, self).__init__("arrows")
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
